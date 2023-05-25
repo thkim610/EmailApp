ff6d6eab9720ed87649aa1798b49b3915bd77ba1
thkim610 <thkim610@naver.com>
2023-05-25 13:15:32 +0900

    메일 앱에 필요한 변수 선언 및 요구사항 주석

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,22 @@
 package emailapp;
 
 public class Email {
-
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//이름 반환
+	
+	//부서 묻기
+	
+	//랜덤으로 비밀번호 생성
+	
+	//메일 용량 설정
+	
+	//메일 주소 변경
+	
+	//비밀번호 변경
 }
