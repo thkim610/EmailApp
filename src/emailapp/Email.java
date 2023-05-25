84e202d6e45745d9d85f16d01a5a70cf94da39d1
thkim610 <thkim610@naver.com>
2023-05-25 13:36:56 +0900

    부서정보입력 기능 추가

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -9,8 +11,30 @@ public class Email {
 	private String alternateEmail;
 	
 	//이름 반환
-	
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("E-MAIL CREATE: " + this.firstName + this.lastName);
+		
+		this.department = setDepartment(); //부서 반환
+		System.out.println("Department: " + this.department);
+	}
 	//부서 묻기
+	private String setDepartment() {
+		System.out.println("*부서정보입력* \n1 직책\n2 직무\n3 이름\n0 없음 \n 부서코드를 입력하세요: ");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if(depChoice == 1) {
+			return "직책";
+		}else if(depChoice == 2) {
+			return "직무";
+		}else if(depChoice == 3) {
+			return "이름";
+		}else {
+			return "";
+		}
+		
+	}
 	
 	//랜덤으로 비밀번호 생성
 	
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -3,7 +3,7 @@ package emailapp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Email em1 = new Email("kim", "Taeho");
 
 	}
 
