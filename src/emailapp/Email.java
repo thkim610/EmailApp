5fa7dceeff980bf91ffb223ae9b6dbea6333e84d
thkim610 <thkim610@naver.com>
2023-05-26 21:40:37 +0900

    랜덤한 비밀번호 생성 기능 추가

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -6,6 +6,7 @@ public class Email {
 	private String firstName;
 	private String lastName;
 	private String password;
+	private int defaultpasswordLength=10;
 	private String department;
 	private int mailboxCapacity;
 	private String alternateEmail;
@@ -16,8 +17,14 @@ public class Email {
 		this.lastName = lastName;
 		System.out.println("E-MAIL CREATE: " + this.firstName + this.lastName);
 		
-		this.department = setDepartment(); //부서 반환
+		//부서 반환
+		this.department = setDepartment(); 
 		System.out.println("Department: " + this.department);
+		
+		//랜덤 비밀번호 생성 메서드
+		this.password = randomPassword(defaultpasswordLength);
+		System.out.println("비밀번호 : " + this.password);
+		
 	}
 	//부서 묻기
 	private String setDepartment() {
@@ -37,7 +44,15 @@ public class Email {
 	}
 	
 	//랜덤으로 비밀번호 생성
-	
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+		char[] password = new char[length]; // 입력받은 비밀번호(길이)를 배열로 저장
+		for(int i = 0; i<length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand); // passwordSet의 문자열 중 랜덤한 문자 하나를 password 배열에 차례대로 저장
+		}
+		return new String (password); //최종 length길이의 비밀번호를 반환
+	}
 	//메일 용량 설정
 	
 	//메일 주소 변경
